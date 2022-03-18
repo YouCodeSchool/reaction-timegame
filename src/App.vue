beb56686420aa79c2d2b95b4f7f3db68e18c67e3
mohamed ibnahmad <ibnahmadmohamed8@gmail.com>
2022-03-18 16:01:02 +0000

    simple game

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+    <h1>Reaction Timer </h1>
+  <button @click="start()" :disabled="isPlaying "> play </button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" @now="timenow"/>
+  <Result v-if="showresults" :score="score" />
+  <!-- <p v-if="showresults">score  dyalk : {{score}} ms</p> -->
+  <!-- <p></p> -->
+</div>
+
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Result from './components/Results';
+export default {
+  name: 'App',
+  components: {Block,Result},
+  data() {
+    return {
+        isPlaying: false,
+        delay:null,
+        score:null,
+        showresults:false,
+        
+    }
+  },
+  methods: {
+    start (){
+      // console.log('you click mr')
+      this.delay =2000 + Math.random() * 5000
+      this.isPlaying=true
+      this.showresults=false
+      // console.log(this.showresults)
+      
+    },
+    endGame (reactTime){
+      this.score=reactTime
+      this.isPlaying=false
+      this.showresults=true
+    }
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background-color: #0faf87;
+  color: white;
+  border: none;
+  padding:8px 16px;
+  letter-spacing:1px;
+  cursor: pointer;
+  margin: 10px;
+  }
+  button[disabled]{
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,70 @@
+<template >
+    <div   class="block" v-if="showBlock" @click="stopTime()">
+        click me
+        <p>{{timenow}} ms</p>
+    </div>
+    
+
+</template>
+
+<script>
+
+
+export default ({
+    
+props:['delay'],
+data (){
+    return{ 
+        showBlock: false,
+        time : null,
+        reactTime:0,
+        timenow : null,
+    }
+},
+mounted(){
+    // console.log("commp")
+    setTimeout(()=>{
+      this.showBlock=true;  
+      this.statTime()
+    
+    },this.delay)
+},
+
+methods: {
+    statTime(){
+        this.time=setInterval(()=>{
+            {this.reactTime+=10
+            this.timenow+= this.reactTime;
+            
+            // console.log(this.reactTime);
+            }
+
+        },10)
+    },
+    stopTime(){
+        clearInterval(this.time)
+        // console.log(this.time)
+        this.$emit('end',this.reactTime)
+    }
+}
+// updated (){
+// console.log('updated')
+    
+// },
+// unmounted (){
+//     console.log('unmounted')
+// }
+    
+})
+</script>
+
+<style scoped>
+.block {
+    width: 400px;
+    border-radius:10px;
+    background-color:#0faf87;
+    color: white;
+    padding:100px 0;
+    margin:40px auto;  
+}
+</style>
